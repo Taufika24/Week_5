c63eeddbfd7272a61d92d1fb39705d379336ba14
Taufika24 <jtaufika243@gmail.com>
2024-11-03 23:31:07 +0530

    Create CheckEach_sumofpair_divisible_by_k.c

---
+++ CheckEach_sumofpair_divisible_by_k.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main() 
+{
+    int n,i,j,k,check=1,sum;
+    printf("Enter size of array");
+    scanf("%d", &n);
+    int a[n];
+    
+    printf("Enter 1st array");
+    for (i = 0; i < n; i++) 
+        scanf("%d", &a[i]);
+    
+    printf("Enter value");
+    scanf("%d",&k);
+    
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(((a[i]+a[j])%k)!=0)
+            {
+                check=0;
+                break;
+                
+            }
+            
+        }
+    }
+    if(check==1)
+        printf("True");
+    if(check==0)
+        printf("False");
+    
+    return 0;
+}
